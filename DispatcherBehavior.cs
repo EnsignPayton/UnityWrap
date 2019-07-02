5e6f94d8d9f3157935193fe6201f610a2b1e2b7f
Logan Payton <Logan.Payton@javs.com>
2019-07-01 23:22:01 -0400

    Implement Simple Dispatcher
    Use `DispatcherBehavior` when you need to access the render thread from
    a background thread. Wrap all the render code in a `Dispatcher.Invoke`
    and it will run on the next *`Update`.
    
    Could technically be used from the render thread, but it's significantly
    less useful in that case.

---
+++ DispatcherBehavior.cs
@@ -0,0 +1,25 @@
+﻿namespace UnityWrap
+{
+    public class DispatcherBehavior : Behavior
+    {
+        public Dispatcher Dispatcher { get; } = new Dispatcher();
+
+        protected override void Update()
+        {
+            Dispatcher.DoWork(DispatcherTiming.Update);
+            base.Update();
+        }
+
+        protected override void FixedUpdate()
+        {
+            Dispatcher.DoWork(DispatcherTiming.FixedUpdate);
+            base.FixedUpdate();
+        }
+
+        protected override void LateUpdate()
+        {
+            Dispatcher.DoWork(DispatcherTiming.LateUpdate);
+            base.LateUpdate();
+        }
+    }
+}
